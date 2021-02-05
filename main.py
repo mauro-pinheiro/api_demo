088fbc2eb9461dd07e954d1f781a4fea2ab83493
Mauro Campos <mauro.pcsergio@gmail.com>
2021-02-05 17:08:50 -0300

    menu financeiro

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Aluno(BaseModel):
+    media: float
+    perc_faltas: float
+    perc_disc_conc: float
+
+class Classficador(BaseModel):
+    class1: float
+    class2: float
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/demo/")
-async def demo(media: float, perc_faltas: float, perc_disc_conc: float):
+async def demoGet(media: float, perc_faltas: float, perc_disc_conc: float):
     return [
         {"class1": 0.01},
         {"class2": 0.09},
     ]
+@app.post("/demo/")
+async def demoPost(aluno: Aluno):
+    return Classficador(0.01, 0.99)
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
